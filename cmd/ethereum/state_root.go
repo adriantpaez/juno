5f2f951330393094ff6074405e2755ab583291a2
kassim <alkassimk@gmail.com>
2022-02-22 09:53:38 +0100

    create config to hold endpoint

--- cmd/ethereum/state_root.go
+++ cmd/ethereum/state_root.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/NethermindEth/juno/configs"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 func GetStateRoot() common.Hash {
-	client, err := ethclient.Dial("")
+	client, err := ethclient.Dial(configs.EthereumNode)
 	if err != nil {
 		log.Fatal(err)
 	}
--- configs/gateway.go
+++ configs/gateway.go
@@ -3,4 +3,5 @@ package configs
 const (
 	GoerliGateway  = "https://alpha4.starknet.io"
 	MainnetGateway = "https://alpha-mainnet.starknet.io"
+	EthereumNode   = "ws://nethermind:ohmahX5joh6ooxa3phei@mainnet-archive.nethermind.io/"
 )
